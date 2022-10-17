f859f503dd7ac77fecfcf4bb194f0f7b304f1499
Grantkite-official <78843534+Grantkite-official@users.noreply.github.com>
2022-10-17 14:43:07 -0700

    Grant- UI

--- Fast Feud 2D/Assets/Scripts/CharacterSelect.cs
+++ Fast Feud 2D/Assets/Scripts/CharacterSelect.cs
@@ -2,34 +2,46 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class CharacterSelect : MonoBehaviour
 {
+
+    public void sceneSelect(){
+
+    }
     public void rand (){
-       
+       int setRand = Random.Range(0,8);
     }
     public void char1 (){
-       
+        //Boxer Jack
+
+       //display char
+       //Store character pngs as masks
+       //pass char1 
     }
+    
     public void char2 (){
-       
+       //king borg
     }
     public void char3 (){
-       
+       //annie P
     }
     public void char4 (){
+        //little C
        
     }
     public void char5 (){
+        //Panda
        
     }
     public void char6 (){
-       
+        //Wendy
     }
     public void char7 (){
-       
+       //Ronnie
     }
     public void char8 (){
-       
+       //SLander
     }
     
 }
