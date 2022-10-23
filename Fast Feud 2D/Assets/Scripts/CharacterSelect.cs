13b7b37af05f376cf5bebf28b6e9be96a0209aec
NevLo <pilleychristian@gmail.com>
2022-10-23 15:28:01 -0700

    CharacterSelect Changes
    added the ability to set characters to players.

--- Fast Feud 2D/Assets/Scripts/CharacterSelect.cs
+++ Fast Feud 2D/Assets/Scripts/CharacterSelect.cs
@@ -5,43 +5,92 @@ using UnityEngine;
 
 public class CharacterSelect : MonoBehaviour
 {
+    public static Character player1 = null;
+    public static Character player2 = null;
+    Character[] charList =
+    {
+        new Jack(),
+        new BurgerKing(),
+        new Annie(),
+        new LilCeaser(),
+        new Panda(),
+        new Wendy(),
+        new Ronnie(),
+        new Colonel(),
+        new Waffle()
+    };
 
     public void sceneSelect(){
 
     }
     public void rand (){
-       int setRand = Random.Range(0,8);
+        int setRand = Random.Range(0,9);
+        BindCharacterToPlayer(setRand);
+        BindCharacterToPlayer(Random.Range(0, 9));
+
     }
+    //implemented
     public void char1 (){
+        BindCharacterToPlayer(0);
         //Boxer Jack
 
-       //display char
-       //Store character pngs as masks
-       //pass char1 
+        //display char
+        //Store character pngs as masks
+        //pass char1 
     }
-    
+    //implemented    
     public void char2 (){
-       //king borg
+        BindCharacterToPlayer(1);
+        //king borg
     }
     public void char3 (){
-       //annie P
+        BindCharacterToPlayer(2);
+        //annie P
     }
     public void char4 (){
+        BindCharacterToPlayer(3);
         //little C
-       
+
     }
     public void char5 (){
+        BindCharacterToPlayer(4);
         //Panda
-       
+
     }
+
+    //implemented 
     public void char6 (){
+        BindCharacterToPlayer(5);
         //Wendy
     }
+
     public void char7 (){
-       //Ronnie
+        BindCharacterToPlayer(6);
+        //Ronnie
     }
     public void char8 (){
-       //SLander
+        BindCharacterToPlayer(7);
+        //SLander
+    }
+
+    // implemented
+    public void char9()
+    {
+        BindCharacterToPlayer(8);
+        //Waffle
     }
     
+    public void BindCharacterToPlayer(int ind)
+    {
+        if (player1 == null)
+        {
+            player1 = charList[ind];
+            Debug.Log("Player 1 has selected " + player1.ToString());
+        }
+        else
+        {
+            player2 = charList[ind];
+            Debug.Log("Player 2 has selcted " + player2.ToString());
+        }
+    }
 }
